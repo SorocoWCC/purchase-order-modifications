31e91b4a16b03f2745cc8852133b0cd29ed95cec
Andrey Castro <a.castro.carvajal@accenture.com>
2017-02-21 19:38:28 -0600

    [purchases] :+1:  Auto product calculation start

--- static/src/js/javascriptfile.js
+++ static/src/js/javascriptfile.js
@@ -1,3 +1,27 @@
 (function() {
+    /*=> Tickets Dashboard Module*/
+    var _model, _view, _controller, body, container,
+    purchaseOrdersModule = {
+        _attachHandlers: function(){
+            /*jQuery(document).on('click', function(e){
 
+            });*/
+        },
+        _detachHandlers: function(){
+
+        },
+        /*Required module functions*/
+        suspend: function(){
+            this._detachHandlers();
+        },
+        init: function(model, view, controller){
+            _model = model; _view = view; _controller = controller;
+            body = window.SOROCOModel.scope.viewBody;
+            container = window.SOROCOModel.scope.viewContainer;
+            console.log('init from purchases module');
+            //this._attachHandlers();
+        }
+        /* End Required module functions*/
+    };
+    window.SOROCOModel.initModule(purchaseOrdersModule, 'th[data-id=amount_total]');
 })(window);
