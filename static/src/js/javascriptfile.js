ea75b691a7eea2503e1fb8bfceb3b4f912c5fd8b
Andrey Castro <a.castro.carvajal@accenture.com>
2017-02-22 18:52:46 -0600

    [JS] :ear:  Added autocalc events

--- static/src/js/javascriptfile.js
+++ static/src/js/javascriptfile.js
@@ -1,45 +1,72 @@
 (function() {
     /*=> Tickets Dashboard Module*/
-    var _model, _view, _controller, body, container, calcButton, formViewWatcher, viewCont,
+    var igniter = window.SOROCOModel, _model, _view, _controller, body, 
+    calcButton, viewCont, formContainer, fullWeightInput, emptyWeightInput,
     purchaseOrdersModule = {
-        container: {},
         _attachHandlers: function() {
-            calcButton.on('click', function(e){
-                console.log('execute calculation');
+
+            /*igniter.moduleContainer.on('viewChange', function() {
+                setTimeout(function(){ 
+                    if (igniter.currentView === 'form') {
+                        let actionBtns = igniter.moduleContainer.find('.oe_stat_button.btn.btn-default.oe_inline:not(.oe_form_invisible)');
+                        
+                        console.log(actionBtns);
+                        jQuery.each(actionBtns, function(index, actionBtn) {
+                            let actBtn = jQuery(actionBtn);
+                            if (actBtn.find('.fa.fa-calculator').length > 0) {
+                                calcButton = actBtn;
+                                calcButton.off('click');
+                                purchaseOrdersModule._initFormElements();
+
+                                calcButton.on('click', function(e){
+                                    purchaseOrdersModule._calculateIron();
+                                });
+                                return;
+                            }
+                        });
+                    }
+                }, 1500);
+            });*/
+
+            igniter.moduleContainer.on('click', '.oe_button.oe_form_button_edit', function() {
+                calcButton = igniter.moduleContainer.find('.oe_stat_button.btn.btn-default.oe_inline.calculatorButton');
+                calcButton.off('click');
+                calcButton.removeClass('disabled');
+                purchaseOrdersModule._initFormElements();
+
+                calcButton.on('click', function(){
+                    purchaseOrdersModule._calculateIron();
+                });
+            });
+
+            igniter.moduleContainer.on('click', '.oe_button.oe_form_button.oe_highlight, .oe_button.oe_form_button_save.oe_highlight', function() {
+                calcButton.addClass('disabled');
             });
         },
         _detachHandlers: function() {
+            igniter.moduleContainer.off('viewChange');
             calcButton.off('click');
         },
-        _waitForFormView: function(){
-            var poModule = this;
-            //Update this functionality for a full view manager, with view change events
-            formViewWatcher = window.setInterval(function() {
-               if (viewCont.attr('data-view-type') === 'form') {
-                    var buttons = viewCont.find('.oe_stat_button.btn.btn-default.oe_inline');
-
-                    calcButton = viewCont.find('.fa.fa-calculator');
-                    console.log('detaching default Handlers');
-                    poModule._detachHandlers();
-                    console.log('attaching Handlers');
-                    poModule._attachHandlers();
-                    window.clearInterval(formViewWatcher);
-               }
-            }, 1500);
+        _initFormElements: function() {
+            formContainer = igniter.moduleContainer.find('oe_formview oe_view');
+            fullWeightInput = formContainer.find('#oe-field-input-4');
+            emptyWeightInput = formContainer.find('#oe-field-input-5');
+            productsRows = formContainer.find('div.oe_list.oe_view.oe_list_editable table.oe_list_content tbody tr[data-id]:not(.oe_form_field_one2many_list_row_add)');
+        },
+        _calculateIron: function(object) {
+            console.log('trigger');
         },
         /*Required module functions*/
         suspend: function() {
-            this._detachHandlers();
+            purchaseOrdersModule._detachHandlers();
         },
         init: function(model, view, controller) {
             _model = model; _view = view; _controller = controller;
-            body = window.SOROCOModel.scope.viewBody;
-            console.log('init from purchases module');
-            viewCont = body.find('.oe_application .oe_view_manager.oe_view_manager_current');
-            this._waitForFormView();
+            body = igniter.scope.viewBody;
+            purchaseOrdersModule._attachHandlers();
             //this._unbindDefaultHandlers();
         }
         /* End Required module functions*/
     };
-    window.SOROCOModel.initModule(purchaseOrdersModule, 'th[data-id=amount_total]');
+    igniter.initModule(purchaseOrdersModule, 'th[data-id=amount_total]');
 })(window);
